15c42c1e15ee8670615beabb0c389312adc6ce21
kacem <kacemmeherzi52@gmail.com>
2024-05-21 00:11:42 +0100

    home , rvlist , patients and doctor routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RvlistComponent } from './rvlist/rvlist.component';
+import { PatientslistComponent } from './patientslist/patientslist.component';
+import { DoctorlistComponent } from './doctorlist/doctorlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'rvlist', component: RvlistComponent },
+    { path: 'patients', component: PatientslistComponent },
+    { path: 'doctors', component: DoctorlistComponent },
+
+
+
+
+];

9d3ca8ed0b1405946d7f11d35b0386cbb107bf6e
kacem <kacemmeherzi52@gmail.com>
2024-05-21 00:12:13 +0100

    httpprovider added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AuthService } from './services/auth.service';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes) , AuthService,provideHttpClient()]
 };

dededc3a2bc0f7ed564acab40543dd59672a3f52
kacem <kacemmeherzi52@gmail.com>
2024-05-21 00:12:58 +0100

    root component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AuthComponent,DashboardComponent,HomeComponent,AuthComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'healthcare_front';
+
+  constructor (private authservice : AuthService) {}
+
+
+ isAuthed() : boolean {
+ return this.authservice.isAuthenticated() }
 }

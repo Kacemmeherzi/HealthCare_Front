db9714c73c0da0822a76b3e99dba3f9358f10deb
kacem <kacemmeherzi52@gmail.com>
2024-05-21 00:06:52 +0100

    rendez vous list component created

---
+++ src/app/rvlist/rvlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rvlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './rvlist.component.html',
+  styleUrl: './rvlist.component.css'
+})
+export class RvlistComponent {
+
+}

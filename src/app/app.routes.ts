409626b904de0aafafede09e2d73be70398ddb4c
kacem <kacemmeherzi52@gmail.com>
2024-05-21 23:53:12 +0100

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { RvlistComponent } from './rvlist/rvlist.component';
 import { PatientslistComponent } from './patientslist/patientslist.component';
 import { DoctorlistComponent } from './doctorlist/doctorlist.component';
+import { AppointmentDoctorComponent } from './appointment-doctor/appointment-doctor.component';
+import { AppointmentPatientComponent } from './appointment-patient/appointment-patient.component';
+import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'rvlist', component: RvlistComponent },
     { path: 'patients', component: PatientslistComponent },
     { path: 'doctors', component: DoctorlistComponent },
+ 
+    { path: 'apdoctor', component: AppointmentDoctorComponent },
+
+    { path: 'appatient', component: AppointmentPatientComponent },
+    { path: 'app-form', component: AppointmentFormComponent },
+    { path: 'user-info', component: UserInfoComponent },
 
 
 
+ 
 
 ];

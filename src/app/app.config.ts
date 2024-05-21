bc5a2616914781925b55beaaeb927e42cf770447
kacem <kacemmeherzi52@gmail.com>
2024-05-21 23:52:54 +0100

    user and appointment services added to the globale provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { AuthService } from './services/auth.service';
 import { provideHttpClient } from '@angular/common/http';
+import { UserService } from './services/user.service';
+import { AppointmentService } from './services/appointment.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) , AuthService,provideHttpClient()]
+  providers: [provideRouter(routes) , AuthService,provideHttpClient(),UserService,AppointmentService]
 };
